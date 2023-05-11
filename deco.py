e50abf133467210fde68a75967e8dd37a9eefe69
David Staab <david.staab@gmail.com>
2023-05-10 22:48:24 -0600

    Added STDIN and STDOUT. Changed cli_app to deco.

---
+++ deco.py
@@ -0,0 +1,225 @@
+import argparse
+import pathlib
+import sys
+import typing as t
+from tempfile import NamedTemporaryFile
+
+from dotenv import load_dotenv
+
+import gcloud_adapter as g
+import openai_adapter as o
+from utils import APP_CONFIG, load_config
+
+load_dotenv()
+
+
+def build_cli() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    input_mutex = parser.add_mutually_exclusive_group()
+    input_mutex.add_argument(
+        '-i', '--in-file',
+        type=str,
+        help='Input file. (If not given, STDIN is used.) Supported formats: ' + ', '.join(o.TRANSCRIBE_FORMATS),
+        )
+    input_mutex.add_argument(
+        '-t', '--transcribe',
+        action='store_true',
+        help='Transcribe binary audio from STDIN. Cannot be used with --in_file. If omitted, STDIN is assumed to carry text.',
+    )
+    parser.add_argument(
+        '-o', '--out-file',
+        type=str,
+        help='Output file. (If not given, STDOUT is used.) Recommend using appropriate suffix for output format: .mp3 or .txt.',
+    )
+    parser.add_argument(
+        '-c', '--config-file',
+        type=str,
+        help='Path to custom config file',
+        )
+    parser.add_argument(
+        '-C', '--no-cleanup',
+        action='store_true',
+        help='Do not cleanup the transcription (if input is audio)',
+    )
+    parser.add_argument(
+        '-O', '--no-optimize',
+        action='store_true',
+        help='Do not optimize the input',
+    )
+    parser.add_argument(
+        '-S', '--no-speech',
+        action='store_true',
+        help='Do not generate speech output. Will output text instead. \
+            MIND THE SUFFIX OF THE OUTPUT FILE, IF YOU SPECIFIED THE "-o" OPTION!',
+    )
+    parser.add_argument(
+        '-x', '--extra-outputs',
+        action='store_true',
+        help='Generate extra outputs from intermediate steps in the processing chain. \
+            Will be stored in the alongside final output file. \
+            Overrides config file settings.',
+    )
+    return parser.parse_args()
+
+
+def override_app_config() -> None:
+    
+    if CLI.config_file:
+        load_config(CLI.config_file)
+    
+    if CLI.extra_outputs:
+        in_file = pathlib.Path(CLI.input)
+        out_trunk = str(in_file.parent.joinpath(in_file.stem).resolve())
+        APP_CONFIG['transcription']['output_file'] = out_trunk + '-trans.txt'
+        APP_CONFIG['cleanup']['output_file'] = out_trunk + '-clean.txt'
+        APP_CONFIG['optimization']['output_file'] = out_trunk + '-optim.txt'
+
+
+def cleanup(text, ) -> str:
+    # TODO Return token count to user for monitoring
+    messages = o.create_chat_messages(
+        APP_CONFIG['cleanup']['prompt'],
+        text,
+        )
+    completion, tokens = o.basic_chat(
+        messages,
+        APP_CONFIG['cleanup']['model'],
+        )
+    return completion
+
+
+def optimize(text: str) -> str:
+    # TODO Return token count to user for monitoring
+    messages = o.create_chat_messages(
+        APP_CONFIG['optimization']['prompt'],
+        text,
+        )
+    completion, tokens = o.basic_chat(
+        messages,
+        APP_CONFIG['optimization']['model'],
+        )
+    
+    output = ''
+    lines = completion.splitlines()
+    for i, line in enumerate(lines):
+        # These strings are taken from the prompt.
+        # TODO Something more robust, since prompt is configurable and
+        #  AI doesn't follow instructions perfectly.
+        if line.startswith('###') and '3' in line:
+            output = '\n'.join(lines[i+1:])
+            break
+    return output
+
+
+def ingest() -> str:
+    
+    if CLI.in_file:
+        in_file = pathlib.Path(CLI.in_file)
+    else:
+        # STDIN - However, o.transcribe() requires a file on disk as input 
+        if CLI.transcribe:
+            stream = sys.stdin.buffer.read()
+            suffix = o.detect_audio_format(stream)
+            temp = NamedTemporaryFile(mode='wb', delete=False, suffix=suffix)
+            temp.write(stream)
+        else:
+            temp = NamedTemporaryFile(mode='w', delete=False, suffix='.txt')
+            try:
+                temp.write(sys.stdin.read())
+            except UnicodeDecodeError as e:
+                raise RuntimeError('Expected UTF-8 text at STDIN but got binary data instead.') from e
+
+        temp.close()
+        in_file = pathlib.Path(temp.name)
+        
+    if in_file.suffix.lstrip('.').lower() in (o.TRANSCRIBE_FORMATS):
+        # Transcribe
+        t_out_file = APP_CONFIG['transcription']['output_file']
+
+        if t_out_file:
+            o.transcribe_to_file(in_file, t_out_file, APP_CONFIG['transcription']['model'])
+            in_text = pathlib.Path(t_out_file).read_text()
+        else:
+            in_text = o.transcribe(in_file, APP_CONFIG['transcription']['model'])
+        
+        if not CLI.in_file:
+            # Delete temporary file  
+            pathlib.Path.unlink(in_file)
+            
+    else:
+        # Ingest
+        in_text = in_file.read_text()
+        
+    return in_text
+
+
+def write_text_output(text: str) -> None:
+    
+    if CLI.out_file:
+        with open(CLI.out_file, mode='w') as f:
+            f.write(text)
+    else:
+        sys.stdout.write(text)
+
+
+def stream_binary_output(first_block: bytes, remainder: t.Generator) -> None:
+    
+    if CLI.out_file:
+        with open(CLI.out_file, mode='wb') as f:
+            f.write(first_block)
+            for block in remainder:
+                f.write(block)
+    else:
+        sys.stdout.buffer.write(first_block)
+        for block in remainder:
+            sys.stdout.buffer.write(block)
+
+
+if __name__ == '__main__':
+    CLI = build_cli()
+    override_app_config()
+    in_text = ingest()
+        
+    # Cleanup
+    if CLI.no_cleanup:
+        c_text = in_text  
+    else:
+        c_text = cleanup(in_text)
+        c_out_file = APP_CONFIG['cleanup']['output_file']
+        
+        if c_out_file:        
+            with open(c_out_file, mode='w') as f:
+                f.write(c_text)
+    
+    # Optimization
+    if CLI.no_optimize:
+        o_text = c_text
+    else:
+        o_text = optimize(c_text)
+        o_out_file = APP_CONFIG['optimization']['output_file']
+        
+        if o_out_file:        
+            with open(o_out_file, mode='w') as f:
+                f.write(o_text)
+    
+    # Speech synthesis
+    if CLI.no_speech:
+        write_text_output(o_text)
+    else:
+        tts = g.GCloudTTS()
+        audio_stream = tts.synthesize_stream(
+            o_text,
+            chunk_size=APP_CONFIG['speech_synthesis']['chunk_size'],
+            )
+        try:
+            chunk0 = next(audio_stream)
+            
+            if len(chunk0):
+                stream_binary_output(chunk0, audio_stream)
+            else:
+                raise ValueError('First chunk of audio stream was size 0.')
+            
+        except (StopIteration, ValueError) as e:
+            raise RuntimeError('Speech synthesis output size was zero. \
+                Possible problem with input. Use "-x" option to view \
+                    intermediate outputs.') from e
--- openai_adapter.py
+++ openai_adapter.py
@@ -7,11 +7,13 @@ from utils import APP_CONFIG
 
 TRANSCRIBE_FORMATS = ['mp3', 'mp4', 'mpeg', 'mpga', 'm4a', 'wav', 'webm']
 
+
 def check_api_up():
     """
     Touches the OpenAI API to make sure everything is configured correctly.
     """
     openai.Model.list()  # Relying on an exception to be thrown if access key not configured or invalid
+
 
 def choose_model(model_name: str='') -> None:
     global chat_model
@@ -28,6 +30,7 @@ def choose_model(model_name: str='') -> None:
             print('Available models:', *models, sep='\n- ')  # Reminder: * unpacks a list or tuple
             once = True
         chat_model = input('Pick one > ' ).strip()
+
 
 def create_chat_messages(prompt: str, text: str, user: str='') -> list[dict]:
 
@@ -47,6 +50,7 @@ def create_chat_messages(prompt: str, text: str, user: str='') -> list[dict]:
 
     return messages
 
+
 def basic_chat(messages: list[dict], model_name: str, temperature: float=0.0, ) -> t.Tuple[str, int]:
     response = openai.ChatCompletion.create(
         model=model_name,
@@ -56,6 +60,7 @@ def basic_chat(messages: list[dict], model_name: str, temperature: float=0.0, ) 
     )
     return response.choices[0].message.content, response.usage.prompt_tokens
 
+
 def adv_chat(messages: list[dict], model_name: str, temperature: float=0.0, ) -> t.Any:
     response = openai.ChatCompletion.create(
         model=model_name,
@@ -64,6 +69,7 @@ def adv_chat(messages: list[dict], model_name: str, temperature: float=0.0, ) ->
         stream=False,
     )
     return response
+
 
 def transcribe(source_file: str|pathlib.Path, model_name: str) -> str:
 
@@ -76,9 +82,36 @@ def transcribe(source_file: str|pathlib.Path, model_name: str) -> str:
         )
     return ret.text
 
+
 def transcribe_to_file(source_file: str|pathlib.Path, output_file: str|pathlib.Path, model_name: str) -> None:
     with open(output_file, mode='w') as f:
         f.write(transcribe(source_file, model_name))
+
+
+def is_mpeg_audio(data: bytes) -> bool:
+    """Check if the data represents an MPEG (MPEG-1, MPEG-2) Layer III audio."""
+    return data.startswith(b'\xff', 0) and (data[1] & 0b11100000) == 0b11100000
+
+
+def detect_audio_format(data: bytes) -> t.Optional[str]:
+    """Detect the audio container format based on the data's signature."""
+    signatures = {
+        b'ID3': ('.mp3', 0),
+        b'\x00\x00\x00 ftypmp4': ('.mp4', 4),
+        b'\x00\x00\x00 ftypM4A': ('.m4a', 4),
+        b'RIFF': ('.wav', 0),
+        b'\x1aE\xdf\xa3': ('.webm', 0),
+        b'fLaC': ('.flac', 0),
+    }
+
+    if is_mpeg_audio(data):
+        signatures[b'\xff'] = ('.mpga', 0)
+
+    for signature, (extension, offset) in signatures.items():
+        if data.startswith(signature, offset):
+            return extension
+
+    return None
 
 
 if __name__ == '__main__':
